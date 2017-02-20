2de3c4895a4eb1d7157c66afb8fdc0ce67287783
Stephanie <steph.crampin@gmail.com>
2017-02-20 10:51:43 +0000

    did exercise 1

--- chap01/ex1.rb
+++ chap01/ex1.rb
@@ -1,1 +1,7 @@
-xxxxx
+puts "Hello WOrld!"
+puts "Hello Again"
+puts "I like typing this."
+puts "This is fun."
+puts "Yay! Printing"
+puts "I'd much rather you 'not'."
+puts 'I "said" do not touch this.'
