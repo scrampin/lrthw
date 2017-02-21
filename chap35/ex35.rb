e39fb9347aedd271dde4867c88685e7d8e004c0f
Stephanie <steph.crampin@gmail.com>
2017-02-21 11:21:37 +0000

    ex35 draft 3

--- chap35/ex35.rb
+++ chap35/ex35.rb
@@ -1,10 +1,10 @@
 def gold_room
 	puts "This room is full of gold. How much do you take?"
 
-	pring "> "
+	print "> "
 	choice = $stdin.gets.chomp
 	
-	if choice.include? "0" || choice.include? "1"
+	if choice.include?('0') || choice.include?('1')
 		how_much = choice.to_i
 	else
 		dead("Man, learn to type a number.")
