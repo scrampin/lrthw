f9e91800dce19a90a008870f535610585ef5606b
Stephanie <steph.crampin@gmail.com>
2017-02-21 14:43:02 +0000

    completed ex40

--- chap39/ex39.rb
+++ chap39/ex39.rb
@@ -11,7 +11,7 @@ states = {
 #create a basic set of states and some cities in them
 
 cities = {
-	'CA' +. 'San Francisco',
+	'CA' => 'San Francisco',
 	'MI' => 'Detroit',
 	'FL' => 'Jacksonville'
 }
