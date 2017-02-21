ae8f1230dd253c5b2cee47c9a9d207b31a5c75f9
Stephanie <steph.crampin@gmail.com>
2017-02-21 10:17:49 +0000

    did ex34

---
+++ chap34/ex34.rb
@@ -0,0 +1,19 @@
+animals = ['bear', 'ruby', 'peacock', 'kangaroo', 'whale', 'platypus']
+
+ruby = animals[1]
+peacock = animals[2]
+bear = animals[0]
+kangaroo = animals[3]
+whale = animals[4]
+peacock = animals[2]
+platypus = animals[5]
+whale = animals[4]
+
+puts ruby
+puts peacock
+puts bear
+puts kangaroo
+puts whale
+puts peacock
+puts platypus
+puts whale
