a1507c9e819010e349a9dd8efd598a703c52d03e
Stephanie <steph.crampin@gmail.com>
2017-02-27 05:20:26 +0000

    finished game draft

--- chap36/start_game.rb
+++ chap36/start_game.rb
@@ -1,3 +1,19 @@
-puts "You decide to go for a run one Sunday morning."
-puts "It is a beautiful day, and there are many options to choose from."
-puts "Do you go to Hyde Park or Victoria Park?"
+
+def start
+  puts "You decide to go for a run one Sunday morning."
+  puts "It is a beautiful day, and there are many options to choose from."
+  puts "Do you go to Hyde Park or Victoria Park?"
+  choice = $stdin.gets.chomp
+  if choice.downcase == "hyde park"
+    Hyde.park
+  elsif choice.downcase == "victoria park"
+    Vic.race
+  else
+    puts "nah there are way better routes than that!"
+  end
+end
+
+start
+
+require "hyde_park"
+require "vic_park"
